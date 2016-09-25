bc56f1455ff61be139716203567b1c789083f856
davinkevin <davin.kevin@gmail.com>
2016-09-25 12:51:12 +0200

    feat(code): use service inside component
    Generate error because not provide to NgModule. Did in purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TimeService} from "./shared/time/time.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hello AngularToulouse!';
+
+  constructor(private timeService:TimeService) {}
+
+  ngOnInit(): void {
+    this.title = this.timeService.getNow();
+  }
+
 }
--- src/app/shared/time/time.service.ts
+++ src/app/shared/time/time.service.ts
@@ -5,4 +5,7 @@ export class TimeService {
 
   constructor() { }
 
+  getNow() {
+    return new Date().toString();
+  }
 }
