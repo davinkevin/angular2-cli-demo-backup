2517fa1126b10b8157c7530bffd0bd427639b48c
davinkevin <davin.kevin@gmail.com>
2016-09-25 12:52:18 +0200

    feat(code): register the service in the provider of the root Module of the app
    This allow the DI to be done and fix the previous bug...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {TimeService} from "./shared/time/time.service";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TimeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
